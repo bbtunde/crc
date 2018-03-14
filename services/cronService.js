f07f7b0d536933ac3d0c995403efb8886003a743
Alade Victor <vstar4all@gmail.com>
2018-03-14 12:38:30 +0100

    [cache service updated]

--- services/cronService.js
+++ services/cronService.js
@@ -18,20 +18,5 @@ var cronTaskPlansDSTV = cron.schedule('0 0,12 * * *', function() {
 }, false);
 
 
-var cronTaskPlansSPECTRANET = cron.schedule('0 0,12 * * *', function() {
-    console.log('Trying to retrieve plans for SPECTRANET...');
-    plansService.getPlans("F3B9CE63-3FB4-468B-83CE-2E3A0E2C8853")
-    .then(plans => {
-        CacheService.set('SPECTRANET', plans);
-        console.log('Plans for SPECTRANET successfully cached!');
-    })
-    .catch(appError => {
-        console.log('Error retrieving plans for SPECTRANET. Plans will not be cached');
-        console.log('Error cause:', appError);
-    }); 
-
-}, false);
-
- 
 module.exports.cronTaskPlansDSTV = cronTaskPlansDSTV;
-module.exports.cronTaskPlansSPECTRANET = cronTaskPlansSPECTRANET;
+

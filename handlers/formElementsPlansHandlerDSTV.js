d5d3ea6e8750cfba88d2873376e1ae4c7e4d8c1c
Alade Victor <vstar4all@gmail.com>
2018-06-08 12:41:20 +0100

    [Dstv ans Smile formhandler modified]

--- handlers/formElementsPlansHandlerDSTV.js
+++ handlers/formElementsPlansHandlerDSTV.js
@@ -17,17 +17,18 @@ module.exports = {
                         if (!cachedPlans) {
                             plansService.getOptionsAndCachePlans('DSTV', linetype)
                                 .then(options => {
-                                   let reFinedOptions=pagaHelpers.addAmountFieldToOption("Box Office",options);
+                                    let _options = JSON.parse(JSON.stringify(options));
+                                    let reFinedOptions=pagaHelpers.addAmountFieldToOption("Box Office",_options);
                                     formElement.elements[1].options = reFinedOptions;
                                     resolve(formElement)
                                 })
                                 .catch(appError => reject(appError));
                         } else {
                             try {
-                                let options = plansService.parsePlansToOptions(cachedPlans);
-                                let reFinedOptions=pagaHelpers.addAmountFieldToOption("Box Office",options);
-                                formElement.elements[1].options = reFinedOptions;
-                                resolve(formElement);
+                                    let _options = JSON.parse(JSON.stringify(options));
+                                    let reFinedOptions=pagaHelpers.addAmountFieldToOption("Box Office",_options);
+                                    formElement.elements[1].options = reFinedOptions;
+                                    resolve(formElement)
                              } catch (error) {
                                 return reject(new AppError(500, ResponseCode.UNKNOWN_ERROR, 'Error ocurred on parsing plans to options', [])); 
                              }
